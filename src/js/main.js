f05cd2303bc75289c76ddb3772b450ff06edb361
BlueTeck <tili2@gmx.de>
2015-07-13 19:01:59 +0200

    change structure + other things

---
+++ src/js/main.js
@@ -0,0 +1,220 @@
+angular.module('project', ['ngRoute', 'ngMaterial', 'ngMdIcons', 'base64'])
+
+
+.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'ProjectListController as projectList',
+      templateUrl: 'view/project_list.html'
+    })
+    .when('/board/show/:projectId/:projectColumn', {
+      controller: 'ShowProjectController as showProject',
+      templateUrl: 'view/board_show.html'
+    })
+    .when('/task/show/:taskId', {
+      controller: 'ShowTaskController as showTask',
+      templateUrl: 'view/task_details.html'
+    })
+    .when('/board/overdue/:projectId', {
+      controller: 'ShowOverdueController as overdueBoard',
+      templateUrl: 'view/board_overdue.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+})
+
+.factory('navigation', ['$location', '$rootScope', function($location, $rootScope) {
+  return {
+    home: function() {
+      $location.path('/');
+      $location.replace();
+      console.log("navi home");
+      return;
+    },
+    task: function(api_id, task_id) {
+      $location.path('/task/show/'+task_id);
+      $location.replace();
+      console.log("navi task");
+      return;
+    }
+  }
+}])
+
+.factory('dataFactory', ['$base64','$http', function($base64, $http) {
+
+  var dataFactory = {};
+  
+  dataFactory.getEndpoints = function(){
+    return [{
+    "name": "Testpage",
+    "token": "09d2645659634f08456dd53fcd12bd0e2122873a6585560ea6d4a3410095",
+    "url": "http://litzbarski.de/todo_dev/jsonrpc.php"
+  }, {
+    "name": "Kanboard.net Demopage",
+    "token": "da2776e2c7ca07b2b1169099550aa4a197024f2f7aac21212682240acc3f",
+    "url": "http://demo.kanboard.net/jsonrpc.php"
+  }];
+  };
+  
+  dataFactory.getBaseUrl = function(api_id) {
+    var api_config = this.getEndpoints()[api_id - 1];
+    return api_config.url;
+  };
+  
+  dataFactory.createConfig = function(api_id) {
+    var api_config = this.getEndpoints()[api_id - 1];
+    var auth = $base64.encode('jsonrpc' + ':' + api_config.token);
+    var config = {
+    headers: {
+      'Authorization': 'Basic ' + auth
+    }
+  };
+    return config;
+  };
+
+  dataFactory.getProjects = function(api_id) {
+    var request = '{"jsonrpc": "2.0", "method": "getAllProjects", "id": 1}';
+    return $http.post(this.getBaseUrl(api_id) + '?getAllProjects', request, this.createConfig(api_id));
+  };
+
+  dataFactory.getBoard = function(api_id, projectid) {
+    var request = '{"jsonrpc": "2.0", "method": "getBoard", "id": 1,"params": { "project_id": ' + projectid + ' }}';
+    return $http.post(this.getBaseUrl(api_id) + '?getBoard', request, this.createConfig(api_id));
+  };
+
+  dataFactory.getProjectById = function(api_id, projectid) {
+    var request = '{"jsonrpc": "2.0", "method": "getProjectById", "id": 1,"params": { "project_id": ' + projectid + ' }}';
+    return $http.post(this.getBaseUrl(api_id) + '?getProjectById', request, this.createConfig(api_id));
+  };
+
+  dataFactory.getProjectActivity = function(api_id, projectid) {
+    var request = '{"jsonrpc": "2.0", "method": "getProjectActivity", "id": 1,"params": { "project_id": ' + projectid + ' }}';
+    return $http.post(this.getBaseUrl(api_id) + '?getProjectActivity', request, this.createConfig(api_id));
+  };
+
+  dataFactory.getTaskById = function(api_id, taskid) {
+    var request = '{"jsonrpc": "2.0", "method": "getTask", "id": 1,"params": { "task_id": ' + taskid + ' }}';
+    return $http.post(this.getBaseUrl(api_id) + '?getTask', request, this.createConfig(api_id));
+  };
+
+  dataFactory.getOverdueTasks = function(api_id) {
+    var request = '{"jsonrpc": "2.0", "method": "getOverdueTasks", "id": 1}';
+    return $http.post(this.getBaseUrl(api_id) + '?getOverdueTasks', request, this.createConfig(api_id));
+  };
+
+
+  return dataFactory;
+}])
+
+.controller('ProjectListController', function($location, $routeParams, $route, $scope, navigation, dataFactory) {
+  $scope.$navigation = navigation;
+  var projectList = this;
+  var api_id = 1;
+  dataFactory.getProjects(api_id)
+    .success(function(request) {
+      projectList.projects = request.result;
+    })
+    .error(function(error) {
+      console.log(error);
+    });
+
+})
+
+.controller('ShowProjectController', function($location, $routeParams, $route, $scope, navigation, dataFactory) {
+    $scope.$navigation = navigation;
+    $scope.project_id = $routeParams.projectId;
+    $scope.column_id = $routeParams.projectColumn;
+
+    var api_id = 1;
+    var project;
+    var board;
+    var numberOfColumns;
+    $scope.tasks = [];
+
+    dataFactory.getProjectById(api_id,$routeParams.projectId)
+      .success(function(request) {
+        project = request.result;
+        $scope.project_name = project.name;
+      })
+      .error(function(error) {
+        console.log(error);
+      });
+
+    dataFactory.getBoard(api_id,$routeParams.projectId)
+      .success(function(request) {
+        board = request.result;
+        numberOfColumns = board[0].columns.length;
+        $scope.column_number = numberOfColumns;
+        $scope.column_name = board[0].columns[$routeParams.projectColumn - 1].title;
+
+        for (var i = 0; i < board.length; i++) {
+          for (var j = 0; j < board[i].columns[$routeParams.projectColumn - 1].tasks.length; j++) {
+            $scope.tasks.push(board[i].columns[$routeParams.projectColumn - 1].tasks[j]);
+          }
+        }
+        //console.log($scope.tasks);
+
+      })
+      .error(function(error) {
+        console.log(error);
+      });
+
+
+    $scope.nextColumn = function() {
+      if (numberOfColumns > $scope.column_id) {
+        $routeParams.projectColumn = $routeParams.projectColumn * 1 + 1;
+        $route.updateParams($routeParams);
+      }
+    }
+
+    $scope.previousColumn = function() {
+      if ($scope.column_id > 1) {
+        $routeParams.projectColumn = $routeParams.projectColumn * 1 - 1;
+        $route.updateParams($routeParams);
+      }
+    }
+
+  })
+  .controller('ShowOverdueController', function($location, $routeParams, $route, $scope, navigation, dataFactory) {
+    $scope.$navigation = navigation;
+    var project_id = $routeParams.projectId;
+
+    var api_id = 1;
+    var overdue;
+
+    $scope.tasks = [];
+
+    dataFactory.getOverdueTasks(api_id)
+      .success(function(request) {
+        overdue = request.result;
+
+        for (var i = 0; i < overdue.length; i++) {
+          if (overdue[i].project_id == project_id) {
+            $scope.tasks.push(overdue[i]);
+            $scope.project_name = overdue[i].project_name;
+          }
+        }
+
+      })
+      .error(function(error) {
+        console.log(error);
+      });
+
+  })
+  .controller('ShowTaskController', function($location, $routeParams, $route, $scope, navigation, dataFactory) {
+    $scope.$navigation = navigation;
+
+    var api_id = 1;
+    var id = $routeParams.taskId;
+    $scope.task;
+
+    dataFactory.getTaskById(api_id,id)
+      .success(function(request) {
+        $scope.task = request.result;
+      })
+      .error(function(error) {
+        console.log(error);
+      });
+
+  });
